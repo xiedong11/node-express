fc7c40d5e4d416a5dd289cc60a142473edd87883
xiedong <xiedong11@aliyun.com>
2019-01-23 13:38:08 +0800

    简单挂载路由

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//新闻列表页
+router.get('/news',function (req,res) {
+  res.send('返回内容为新闻列表')
+})
+
 module.exports = router;
